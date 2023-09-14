4817f0bc11d9ebb0765a5e67a483e9f11b328626
satyamkumarjha9966 <satyamkumarjha9966@gmail.com>
2023-09-14 20:01:53 +0530

    Added CourseSlice

---
+++ client/src/Redux/Slices/CourseSlice.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+import axiosInstance from "../../Helpers/axiosInstance";
+
+const initialState = {
+  courseData: [],
+};
+
+export const getAllCourses = createAsyncThunk("/course/get", async () => {
+  try {
+    const response = axiosInstance.get("/courses");
+    toast.promise(response, {
+      loading: "Loading Course Data....",
+      success: "Course Loaded Successfully",
+      error: "Failed to get the Courses",
+    });
+    return (await response).data.courses;
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+  }
+});
+
+const courseSlice = createSlice({
+  name: "courses",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default courseSlice.reducer;
--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -1,53 +1,53 @@
-import { Router } from 'express';
-import { getAllCourses, getLecturesByCourseId, createCourse, updateCourse, removeCourse, addLectureToCourseById, updateLectureOfCourseById, deleteLectureOfCourseById } from '../controllers/course.controller.js';
-import { authorizeSubscriber, authorizedRoles, isLoggedIn } from '../middlewares/auth.middleware.js';
+import { Router } from "express";
+import {
+  getAllCourses,
+  getLecturesByCourseId,
+  createCourse,
+  updateCourse,
+  removeCourse,
+  addLectureToCourseById,
+  updateLectureOfCourseById,
+  deleteLectureOfCourseById,
+} from "../controllers/course.controller.js";
+import {
+  authorizeSubscriber,
+  authorizedRoles,
+  isLoggedIn,
+} from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 
 const courseRouter = Router();
 
-courseRouter.route('/')            // From this we can made multiple types of request once
-    .get(getAllCourses)
-    .post(
-        isLoggedIn,
-        authorizedRoles('ADMIN'),
-        upload.single("thumbnail"),      // From this we can get data in req.body although we send data in form data and content-type is multipart/form-data
-        createCourse
-        );
+courseRouter
+  .route("/") // From this we can made multiple types of request once
+  .get(getAllCourses)
+  .post(
+    isLoggedIn,
+    authorizedRoles("ADMIN"),
+    upload.single("thumbnail"), // From this we can get data in req.body although we send data in form data and content-type is multipart/form-data
+    createCourse
+  );
 
-courseRouter.route('/:id')
-    .get(
-        isLoggedIn, 
-        authorizeSubscriber,
-        getLecturesByCourseId
-        )
-    .put(
-        isLoggedIn, 
-        authorizedRoles('ADMIN'),
-        upload.single("thumbnail"),
-        updateCourse
-        )
-    .delete(
-        isLoggedIn, 
-        authorizedRoles('ADMIN'), 
-        removeCourse
-        )
-    .post(
-        isLoggedIn, 
-        authorizedRoles('ADMIN'),
-        upload.single("thumbnail"),
-        addLectureToCourseById
-        )
+courseRouter
+  .route("/:id")
+  .get(isLoggedIn, authorizeSubscriber, getLecturesByCourseId)
+  .put(
+    isLoggedIn,
+    authorizedRoles("ADMIN"),
+    upload.single("thumbnail"),
+    updateCourse
+  )
+  .delete(isLoggedIn, authorizedRoles("ADMIN"), removeCourse)
+  .post(
+    isLoggedIn,
+    authorizedRoles("ADMIN"),
+    upload.single("thumbnail"),
+    addLectureToCourseById
+  );
 
-    courseRouter.route('/:id1/:id2')
-        .put(
-            isLoggedIn, 
-            authorizedRoles('ADMIN'),
-            updateLectureOfCourseById
-        )
-        .delete(
-            isLoggedIn, 
-            authorizedRoles('ADMIN'),
-            deleteLectureOfCourseById
-        )
+courseRouter
+  .route("/:id1/:id2")
+  .put(isLoggedIn, authorizedRoles("ADMIN"), updateLectureOfCourseById)
+  .delete(isLoggedIn, authorizedRoles("ADMIN"), deleteLectureOfCourseById);
 
 export default courseRouter;
