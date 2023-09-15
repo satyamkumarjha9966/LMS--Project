01056e1d1c4cfbbd034ff8459ababceea5118798
satyamkumarjha9966 <satyamkumarjha9966@gmail.com>
2023-09-15 09:53:20 +0530

    Added Course List Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import AboutUsPage from "./Pages/AboutUsPage";
 import PageNotFoundPage from "./Pages/PageNotFoundPage";
 import SignupPage from "./Pages/SignupPage";
 import LoginPage from "./Pages/LoginPage";
+import CourseList from "./Pages/Course/CourseList";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUsPage />}></Route>
+        <Route path="/courses" element={<CourseList />}></Route>
 
         <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
---
+++ client/src/Pages/Course/CourseList.jsx
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCourses } from "../../Redux/Slices/CourseSlice";
+import { useEffect } from "react";
+import HomeLayout from "../../Layouts/HomeLayout";
+import CourseCard from "../../Components/CourseCard";
+
+function CourseList() {
+  const dispatch = useDispatch();
+  const { courseData } = useSelector((state) => state.course);
+
+  async function loadCourses() {
+    await dispatch(getAllCourses());
+  }
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+
+  return (
+    <HomeLayout>
+      <div className="min-h-[90vh] pt-12 pl-20 flex flex-col gap-10">
+        <h1 className="text-center text-3xl font-bold">
+          Explore the Courses made by
+          <span className="font-bold text-orange-500"> Industry Experts</span>
+        </h1>
+        <div className="mb-10 flex flex-wrap gap-14">
+          {courseData?.map((element) => {
+            return <CourseCard key={element._id} data={element} />;
+          })}
+        </div>
+      </div>
+    </HomeLayout>
+  );
+}
+
+export default CourseList;
